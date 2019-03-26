6d15cfa7c9f6ec43dad2e2108d881c553b6a7575
Anton Dorofeyev <anton@morning.agency>
2019-03-26 21:12:24 +0200

    minor refactoring

--- src/components/animated-counter/animated-counter.jsx
+++ src/components/animated-counter/animated-counter.jsx
@@ -1,24 +1,20 @@
-/* eslint-disable */
 import './IG-sample/ig-sample.css';
 
 import React, { Component } from 'react';
 
-if (!window.Lottie) {
-    window.Lottie = require('./IG-sample/IG');
-}
-
 class AnimatedCounter extends Component {
 
+    lottiePlayer = require('./IG-sample/IG');
+    animationData = require('./IG-sample/data.json');
     animation = {};
     percentageInput = React.createRef();
-    animationData = require('./IG-sample/data.json');
+    animationContainer = React.createRef();
 
     play = (percentage) => {
         if (!this.animation.playSegments) return;
 
         let stopFrame = percentage;
         let startFrame = (percentage > 50) ? percentage - 30 : 0;
-
         this.animation.playSegments([startFrame, stopFrame + 1]);
     }
 
@@ -29,22 +25,22 @@ class AnimatedCounter extends Component {
     componentDidMount() {
 
         let params = {
-            container: window.lottie,
+            container: this.animationContainer.current,
             renderer: 'svg',
             loop: false,
             autoplay: false,
             animationData: this.animationData
         };
 
-        this.animation = window.Lottie.loadAnimation(params);
+        this.animation = this.lottiePlayer.loadAnimation(params);
     }
 
     render() {
         return (
             <div className="animated-counter-container">
-                <div id="lottie"></div>
+                <div id="lottie" ref={ this.animationContainer }></div>
                 <div className="btnWrap">
-                    <input type="text" ref={ this.percentageInput } />
+                    <input placeholder="enter % value" type="text" ref={ this.percentageInput } />
                     <button onClick={ this.playSelectedPercentage }>play</button>
                 </div>
             </div>
